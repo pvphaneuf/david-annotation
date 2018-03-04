0bbd3b11dfcf09ed3b2389df45ff736fec081d91
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2018-03-04 00:57:40 +0000

    latest testing of DAVID API

--- chart_report.py
+++ chart_report.py
@@ -6,50 +6,64 @@ import os
 
 from suds.client import Client
 
-USER = 'user@email.org'
+USER = 'pphaneuf@eng.ucsd.edu'
 
 
-def david_enrich(list_file, id_type, bg_file=None, res_file=None,
-                 bg_name='Background1', list_name='List1', category=None,
-                 thd=0.1, count=2):
+def david_enrich(list_file,
+                 id_type,
+                 bg_file=None,
+                 res_file=None,
+                 bg_name='Background1',
+                 list_name='List1',
+                 category=None,
+                 thd=0.1,
+                 count=2):
     """Get chart report and write it to file."""
 
     if list_file and os.path.exists(list_file):
         input_list_ids = ','.join(open(list_file).read().split('\n'))
-        print 'List loaded.'
+        print('List loaded.')
     else:
-        print 'No list loaded.'
+        print('No list loaded.')
         return
 
     flag_bg = False
     if bg_file and os.path.exists(bg_file):
         input_bg_ids = ','.join(open(bg_file).read().split('\n'))
         flag_bg = True
-        print 'Use file background.'
+        print('Use file background.')
     else:
-        print 'Use default background.'
+        print('Use default background.')
 
     client = Client(
-        'https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService?'
+        'https://david.ncifcrf.gov/webservice/services/DAVIDWebService?'
         'wsdl')
     client.wsdl.services[0].setlocation(
-        'https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService.'
+        'https://david.ncifcrf.gov/webservice/services/DAVIDWebService.'
         'DAVIDWebServiceHttpSoap11Endpoint/')
-    print 'User Authentication:', client.service.authenticate(
-        USER)
+    print('User Authentication:', client.service.authenticate(USER))
 
     list_type = 0
-    print 'Percentage mapped(list):', client.service.addList(
-        input_list_ids, id_type, list_name, list_type)
+    print('Percentage mapped(list):', client.service.addList(
+        input_list_ids, id_type, list_name, list_type))
     if flag_bg:
         list_type = 1
-        print 'Percentage mapped(background):', client.service.addList(
-            input_bg_ids, id_type, bg_name, list_type)
+        print('Percentage mapped(background):', client.service.addList(
+            input_bg_ids, id_type, bg_name, list_type))
 
-    print 'Use categories:', client.service.setCategories(category)
+    '''
+    print('Use categories:', client.service.setCategories(category))
     chart_report = client.service.getChartReport(thd, count)
-    print 'Total chart records:', len(chart_report)
+    print('Total chart records:', len(chart_report))
+    '''
+    
+    print('Use categories:', client.service.setCategories(category))
+    chart_report = client.service.getTermClusterReport(3,2,2,0.5,30)
+    print('Total chart records:', len(chart_report))
+    print(chart_report)
+    
 
+    '''
     if not res_file or not os.path.exists(res_file):
         if flag_bg:
             res_file = list_file + '.withBG.chart_report.csv'
@@ -67,9 +81,11 @@ def david_enrich(list_file, id_type, bg_file=None, res_file=None,
         writer.writeheader()
         for row in chart_report:
             writer.writerow(dict(row))
-        print 'write file:', res_file, 'finished!'
+        print('write file:', res_file, 'finished!')
+        '''
 
+        
 if __name__ == '__main__':
-    david_enrich(list_file='./list1.txt', id_type='ENSEMBL_GENE_ID',
-                 list_name='list1',
-                 category='abcd,GOTERM_BP_FAT')
+    david_enrich(list_file='./list1.txt',
+                 id_type='AFFYMETRIX_3PRIME_IVT_ID',
+                 list_name='list1',category='abcd,BBID,BIOCARTA,COG_ONTOLOGY,INTERPRO,KEGG_PATHWAY,OMIM_DISEASE,PIR_SUPERFAMILY,SMART,SP_PIR_KEYWORDS,UP_SEQ_FEATURE')
